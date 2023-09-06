cf2d55e915855fe4f31f1f70df73e0e4be9500df
Jeans <sistemadesignstyle@gmail.com>
2023-09-06 00:41:33 -0500

    Creando variables de colores tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     container: {
       center: true,
     },
-    extend: {},
+    extend: {
+      colors: {
+        azul: "#5394cf",
+        celeste: "#75B0E0",
+        amarillo: "#ECCD50",
+        celeste_claro: "#D8EAF4",
+      },
+    },
   },
   plugins: [],
 };

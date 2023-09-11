b5212c066b66a41997768c75a9a573ca24dad2db
Jeans <sistemadesignstyle@gmail.com>
2023-09-11 00:07:32 -0500

    Peticion Http Fetch Get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'hola mundo';
+
+  ngOnInit(): void {
+    console.log('Inicio del Proyecto');
+    this.getPost();
+  }
+
+  /*   getPost() {
+    fetch('http://localhost/back_legajo/public/api/test_api').then((response) =>
+      console.log(response)
+    );
+  } */
+
+  getPost() {
+    fetch('http://localhost/back_legajo/public/api/test_api')
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
 }

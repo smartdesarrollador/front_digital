4c59505a3e4e2632ae5e3648df37e51d3a96416f
Jeans <sistemadesignstyle@gmail.com>
2023-09-28 01:51:00 -0500

    agregando elementos a componentes

--- src/app/paginas/contratacion/contrato/sexto-proceso/sexto-proceso.component.ts
+++ src/app/paginas/contratacion/contrato/sexto-proceso/sexto-proceso.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-sexto-proceso',
   templateUrl: './sexto-proceso.component.html',
-  styleUrls: ['./sexto-proceso.component.css']
+  styleUrls: ['./sexto-proceso.component.css'],
 })
 export class SextoProcesoComponent {
+  //declarar variable el cual se le da el valor de la fecha actual
+  fechaActual = new Date();
 
+  //declarar dia mes y año de fechaActual
+  dia = this.fechaActual.getDate();
+  mes = this.fechaActual.getMonth() + 1;
+  anio = this.fechaActual.getFullYear();
+
+  //declarar variable para almacenar la fecha actual con el formato dd/MM/yyyy
+  fechaActualFormato: string = this.dia + '/' + this.mes + '/' + this.anio;
+  fechaRenovacion: string = this.dia + '/' + this.mes + '/' + (this.anio + 1);
 }

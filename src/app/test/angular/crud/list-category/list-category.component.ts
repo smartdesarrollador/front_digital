bd9ef6e416924a01688b0743beadefa0de72bac1
Jeans <sistemadesignstyle@gmail.com>
2023-10-02 00:06:43 -0500

    Test Crud

--- src/app/test/angular/crud/list-category/list-category.component.ts
+++ src/app/test/angular/crud/list-category/list-category.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
+import { Category } from '../../interfaces/category';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-category',
   templateUrl: './list-category.component.html',
   styleUrls: ['./list-category.component.css'],
 })
-export class ListCategoryComponent {
+export class ListCategoryComponent implements OnInit {
   listCategories: any = [];
 
-  constructor(public cs: CategoryService) {}
+  constructor(public cs: CategoryService, private router: Router) {}
 
   ngOnInit(): void {
     this.loadCategories();
@@ -19,6 +21,18 @@ export class ListCategoryComponent {
     return this.cs.getCategories().subscribe((data: {}) => {
       console.log(data);
       this.listCategories = data;
+    });
+  }
+
+  onEdit(category: Category) {
+    console.log(category);
+    this.cs.selectCategory = Object.assign({}, category);
+    this.router.navigate(['/test/angular/crud/add-category']);
+  }
+
+  onDelete(id: number) {
+    this.cs.deleteCategory(id).subscribe((response) => {
+      this.loadCategories();
     });
   }
 }
--- src/app/test/angular/services/category.service.ts
+++ src/app/test/angular/services/category.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Category } from '../interfaces/category';
 
@@ -7,11 +7,31 @@ import { Category } from '../interfaces/category';
   providedIn: 'root',
 })
 export class CategoryService {
-  url = 'http://localhost/back_legajo/public/api/test_api_crud';
+  url = 'http://127.0.0.1:8000/api/test_api_crud';
+
+  selectCategory: Category = new Category();
+
+  reqHeader = new HttpHeaders({
+    'Content-Type': 'aplication/json',
+  });
 
   constructor(private http: HttpClient) {}
 
   getCategories(): Observable<Category> {
     return this.http.get(this.url);
+  }
+
+  createCategory(category: Category): Observable<Category> {
+    return this.http.post(this.url, category, { headers: this.reqHeader });
+  }
+
+  updateCategory(id: number, category: Category) {
+    return this.http.put(this.url + '/' + id + '/', category, {
+      headers: this.reqHeader,
+    });
+  }
+
+  deleteCategory(id: number) {
+    return this.http.delete(this.url + '/' + id + '/');
   }
 }

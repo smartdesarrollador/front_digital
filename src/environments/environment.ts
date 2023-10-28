5091be0f40c22551a8758562ee04dd73fd01ad40
smart <sistemadesignstyle@gmail.com>
2023-10-28 11:34:38 -0500

    environment url trabajador

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://back.iatecdigital.com/api/test_api_crud',
   apiUrl2: 'https://back.iatecdigital.com/api/test_api',
+  apiUrl3: 'https://back.iatecdigital.com/api/file',
+  apiUrlTrabajador: 'https://back.iatecdigital.com/api/trabajador',
 };

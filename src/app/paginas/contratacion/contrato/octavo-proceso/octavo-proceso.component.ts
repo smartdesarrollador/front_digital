c63564693d95240391bf7b1dbd238dd7ec8c4748
smart <sistemadesignstyle@gmail.com>
2023-10-30 15:49:28 -0500

    avance octavo proceso

--- src/app/paginas/contratacion/contrato/octavo-proceso/octavo-proceso.component.ts
+++ src/app/paginas/contratacion/contrato/octavo-proceso/octavo-proceso.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { TrabajadorService } from 'src/app/services/trabajador.service';
+import { Trabajador } from 'src/app/interface/trabajador';
+import { Router } from '@angular/router';
+import { ContratoLocalStorageService } from 'src/app/services/localstorage/contrato-local-storage.service';
 
 @Component({
   selector: 'app-octavo-proceso',
@@ -6,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./octavo-proceso.component.css']
 })
 export class OctavoProcesoComponent {
+  trabajador_confianza: boolean = false;
+  trabajador_direccion: boolean = false;
+
+  constructor(
+    public ts: TrabajadorService,
+    private router: Router,
+    private cl: ContratoLocalStorageService
+  ) { }
+
+  saveToLocalStorage() {
+    const contratoLocaldatos = this.cl.getItem('contratoLocal');
+    contratoLocaldatos.trabajador_confianza = this.trabajador_confianza;
+    contratoLocaldatos.trabajador_direccion = this.trabajador_direccion;
+
+
+    this.cl.setItem('contratoLocal', contratoLocaldatos);
+
+    /* localStorage.setItem('selectedValue', this.selectedValue); */
+
+    this.router.navigate(['/contratacion/contrato/proceso_9']);
+  }
+
+
+
+
 
 }
--- src/app/paginas/contratacion/contrato/septimo-proceso/septimo-proceso.component.ts
+++ src/app/paginas/contratacion/contrato/septimo-proceso/septimo-proceso.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { TrabajadorService } from 'src/app/services/trabajador.service';
+import { Trabajador } from 'src/app/interface/trabajador';
+import { Router } from '@angular/router';
+import { ContratoLocalStorageService } from 'src/app/services/localstorage/contrato-local-storage.service';
 
 @Component({
   selector: 'app-septimo-proceso',
@@ -7,8 +11,20 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./septimo-proceso.component.css'],
 })
 export class SeptimoProcesoComponent {
+
+  selectedValue: string = '';
   //declarar variable el cual se le da el valor de la fecha actual
   fechaActual = new Date();
+  remuneracion: string = '';
+  duracion_contrato: number = 1;
+  fecha_periodo: string = '';
+
+  constructor(
+    public ts: TrabajadorService,
+    private router: Router,
+    private cl: ContratoLocalStorageService
+  ) { }
+
 
   //declarar dia mes y año de fechaActual
   dia = this.fechaActual.getDate();
@@ -18,4 +34,19 @@ export class SeptimoProcesoComponent {
   //declarar variable para almacenar la fecha actual con el formato dd/MM/yyyy
   fechaActualFormato: string = this.dia + '/' + this.mes + '/' + this.anio;
   fechaRenovacion: string = this.dia + '/' + this.mes + '/' + (this.anio + 1);
+
+  saveToLocalStorage() {
+    const contratoLocaldatos = this.cl.getItem('contratoLocal');
+    contratoLocaldatos.fecha_inicio = this.fechaActualFormato;
+    contratoLocaldatos.fecha_renovacion = this.fechaRenovacion;
+    contratoLocaldatos.remuneracion = this.remuneracion;
+    contratoLocaldatos.duracion_contrato = this.duracion_contrato + ' ' + this.fecha_periodo;
+
+
+    this.cl.setItem('contratoLocal', contratoLocaldatos);
+
+    /* localStorage.setItem('selectedValue', this.selectedValue); */
+
+    this.router.navigate(['/contratacion/contrato/proceso_8']);
+  }
 }

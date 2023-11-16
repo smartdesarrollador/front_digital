6cf6df5f37cef6df17a8bd514a92a77edca77290
Jeans <sistemadesignstyle@gmail.com>
2023-11-16 01:23:11 -0500

    creando validaciones register

--- src/app/paginas/login/login.module.ts
+++ src/app/paginas/login/login.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, LoginRoutingModule, FormsModule, ReactiveFormsModule],
 })
-export class LoginModule { }
+export class LoginModule {}
--- src/app/paginas/login/register/register.component.ts
+++ src/app/paginas/login/register/register.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+  submitted = false;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: [null, Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  get name() {
+    return this.form.get('name') as FormControl;
+  }
+
+  get email() {
+    return this.form.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.form.get('password') as FormControl;
+  }
+
+  get confirmPassword() {
+    return this.form.get('confirmPassword') as FormControl;
+  }
+
+  /*   get f() {
+    return this.form.controls;
+  } */
+
+  submit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+  }
 }

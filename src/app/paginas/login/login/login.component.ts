5058d5e4616742b1a1ba30d891e012004a2b2422
siste <galaxiaestrellaxl@hotmail.com>
2023-11-17 18:13:03 -0500

    validaciones formulario login

--- src/app/paginas/login/login/login.component.ts
+++ src/app/paginas/login/login/login.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +13,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  form: FormGroup;
+  submitted = false;
+  data:any;
 
+  constructor(private formBuilder: FormBuilder, private toastr: ToastrService) {
+    this.form = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+      }
+    );
+  }
+
+  ngOnInit(): void {}
+
+  get email() {
+    return this.form.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.form.get('password') as FormControl;
+  }
+
+  submit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+
+  
+  }
 }

b1ab2b76a90804dd909dbcbdac835f685ef0eb41
Jeans <sistemadesignstyle@gmail.com>
2023-12-28 01:02:25 -0500

    realizando configuraciones

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const environment = {
+/* export const environment = {
   production: true,
   apiUrl: 'https://apilegajo.amorinomoda.com/api/test_api_crud',
   apiUrl2: 'https://apilegajo.amorinomoda.com/api/test_api',
@@ -12,4 +12,19 @@ export const environment = {
     'https://apilegajo.amorinomoda.com/api/ultimo_empleador',
   apiUrlActividadUltimoEmpleador:
     'https://apilegajo.amorinomoda.com/api/actividad_ultimo_empleador',
+}; */
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://back.iatecdigital.com/api/test_api_crud',
+  apiUrl2: 'https://back.iatecdigital.com/api/test_api',
+  apiUrl3: 'https://back.iatecdigital.com/api/file',
+  apiUrlTrabajador: 'https://back.iatecdigital.com/api/trabajador',
+  apiUrlRegister: 'https://back.iatecdigital.com/api/register',
+  apiUrlLogin: 'https://back.iatecdigital.com/api/login',
+  apiUrlEmail: 'https://back.iatecdigital.com/api/enviar_correo',
+  apiUrlEmpleador: 'https://back.iatecdigital.com/api/empleador',
+  apiUrlUltimoEmpleador: 'https://back.iatecdigital.com/api/ultimo_empleador',
+  apiUrlActividadUltimoEmpleador:
+    'https://back.iatecdigital.com/api/actividad_ultimo_empleador',
 };

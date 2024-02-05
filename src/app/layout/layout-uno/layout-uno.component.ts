445d24325affcbece164accad03153bfffda7582
Jeans <sistemadesignstyle@gmail.com>
2024-02-04 23:55:16 -0500

    componente dropdown basico

--- src/app/layout/layout-uno/layout-uno.component.ts
+++ src/app/layout/layout-uno/layout-uno.component.ts
@@ -1,18 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarBasicoComponent } from '../componentes/navbar-basico/navbar-basico.component';
-import { initFlowbite } from 'flowbite';
+import { DropdownBasicoComponent } from '../componentes/dropdown-basico/dropdown-basico.component';
 
 @Component({
   selector: 'app-layout-uno',
   standalone: true,
-  imports: [NavbarBasicoComponent],
+  imports: [NavbarBasicoComponent, DropdownBasicoComponent],
   templateUrl: './layout-uno.component.html',
   styleUrl: './layout-uno.component.css',
 })
 export class LayoutUnoComponent implements OnInit {
-  title = 'web-app';
-
-  ngOnInit(): void {
-    initFlowbite();
-  }
+  ngOnInit(): void {}
 }

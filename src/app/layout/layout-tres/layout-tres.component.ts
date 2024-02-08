7f524009fd30a3fba1ef46f26eaecd1a0d6fedde
Jeans <sistemadesignstyle@gmail.com>
2024-02-08 13:58:57 -0500

    ejemplo toast

--- src/app/layout/layout-tres/layout-tres.component.ts
+++ src/app/layout/layout-tres/layout-tres.component.ts
@@ -2,12 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Footer1Component } from '../componentes/footer-1/footer-1.component';
 import { SidebarDashboardComponent } from '../componentes/sidebar-dashboard/sidebar-dashboard.component';
 import { Table1Component } from '../componentes/table-1/table-1.component';
+import { Toast1Component } from '../componentes/toast-1/toast-1.component';
 import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-layout-tres',
   standalone: true,
-  imports: [Footer1Component, SidebarDashboardComponent, Table1Component],
+  imports: [
+    Footer1Component,
+    SidebarDashboardComponent,
+    Table1Component,
+    Toast1Component,
+  ],
   templateUrl: './layout-tres.component.html',
   styleUrl: './layout-tres.component.css',
 })

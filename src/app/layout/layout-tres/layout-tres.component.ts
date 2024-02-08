53fbcf4b2d80dd5425cc6cf931737555b39d9092
Jeans <sistemadesignstyle@gmail.com>
2024-02-08 13:49:56 -0500

    ejemplo table

--- src/app/layout/layout-tres/layout-tres.component.ts
+++ src/app/layout/layout-tres/layout-tres.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Footer1Component } from '../componentes/footer-1/footer-1.component';
 import { SidebarDashboardComponent } from '../componentes/sidebar-dashboard/sidebar-dashboard.component';
+import { Table1Component } from '../componentes/table-1/table-1.component';
 import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-layout-tres',
   standalone: true,
-  imports: [Footer1Component, SidebarDashboardComponent],
+  imports: [Footer1Component, SidebarDashboardComponent, Table1Component],
   templateUrl: './layout-tres.component.html',
   styleUrl: './layout-tres.component.css',
 })

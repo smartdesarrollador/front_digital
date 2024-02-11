2fc8368973213ade12e693b80912a78665f51ffd
Jeans <sistemadesignstyle@gmail.com>
2024-02-11 14:40:17 -0500

    card info

--- src/app/pages/vista/inicio/inicio.component.ts
+++ src/app/pages/vista/inicio/inicio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Jumbotron1Component } from 'src/app/layout/componentes/jumbotron-1/jumbotron-1.component';
+import { CardInfoComponent } from 'src/app/layout/componentes/card-info/card-info.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [Jumbotron1Component],
+  imports: [Jumbotron1Component, CardInfoComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
 })

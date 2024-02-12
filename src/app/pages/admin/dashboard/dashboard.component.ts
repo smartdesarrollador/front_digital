3b4ddad6ccca16bf8bed7691bfcb581605711372
Jeans <sistemadesignstyle@gmail.com>
2024-02-11 22:50:52 -0500

    modificaciones componentes

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { PermissionDirective } from 'src/app/directives/permission.directive';
+import { Toast1Component } from 'src/app/layout/componentes/toast-1/toast-1.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, PermissionDirective],
+  imports: [CommonModule, PermissionDirective, Toast1Component],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })

db891b716bf7fa1d7354638b554313977c97223b
Jeans <sistemadesignstyle@gmail.com>
2024-02-13 16:12:07 -0500

    ejemplo services

--- src/app/pages/vista/inicio/inicio.component.ts
+++ src/app/pages/vista/inicio/inicio.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { Jumbotron1Component } from 'src/app/layout/componentes/jumbotron-1/jumbotron-1.component';
 import { CardInfoComponent } from 'src/app/layout/componentes/card-info/card-info.component';
 import { Feature1Component } from 'src/app/layout/componentes/feature-1/feature-1.component';
+import { Services1Component } from 'src/app/layout/componentes/services-1/services-1.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [Jumbotron1Component, CardInfoComponent, Feature1Component],
+  imports: [
+    Jumbotron1Component,
+    CardInfoComponent,
+    Feature1Component,
+    Services1Component,
+  ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
 })

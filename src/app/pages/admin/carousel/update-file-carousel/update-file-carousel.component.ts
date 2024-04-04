710860c372b60d3c0b6c10d798b13fca4afc496e
Jeans <sistemadesignstyle@gmail.com>
2024-04-04 12:10:47 -0500

    crud carousel

--- src/app/pages/admin/carousel/update-file-carousel/update-file-carousel.component.ts
+++ src/app/pages/admin/carousel/update-file-carousel/update-file-carousel.component.ts
@@ -1,12 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Upload } from 'src/app/models/upload.model';
+import { CarouselService } from 'src/app/services/carousel.service';
+import { CommonModule } from '@angular/common';
+import {
+  HttpClient,
+  HttpEvent,
+  HttpRequest,
+  HttpResponse,
+  HttpClientModule,
+} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-update-file-carousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './update-file-carousel.component.html',
-  styleUrl: './update-file-carousel.component.css'
+  styleUrl: './update-file-carousel.component.css',
 })
-export class UpdateFileCarouselComponent {
+export class UpdateFileCarouselComponent implements OnInit {
+  listCategories: any = [];
+  files: any;
+  submitted = false;
+  data: any;
+  form: FormGroup = new FormGroup({});
+  urlRaiz = environment.urlRaiz + '/';
+  valor_id_carousel: any;
+  post = new Upload();
+  constructor(
+    private formBuilder: FormBuilder,
+    private dataService: CarouselService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    this.createForm();
+    this.loadCategories();
+
+    this.route.queryParams.subscribe((params) => {
+      const categoryId = params['categoryId'];
+      this.valor_id_carousel = categoryId;
+    });
+  }
+
+  loadCategories() {
+    return this.dataService.getCategories().subscribe((data: {}) => {
+      console.log(data);
+      this.listCategories = data;
+    });
+  }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      image: [null, Validators.required],
+    });
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  uploadImage(event: Event) {
+    if (event.target instanceof HTMLInputElement) {
+      if (event.target.files && event.target.files.length > 0) {
+        this.files = event.target.files[0];
+        console.log(this.files);
+      } else {
+        console.log('no se selecciono ningun archivo');
+      }
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('nombre_carousel', this.files, this.files.name);
+
+    formData.append('id_carousel', this.valor_id_carousel);
+
+    this.dataService.updateData(formData).subscribe((res) => {
+      this.data = res;
+      console.log(this.data);
+      this.alerta();
+    });
+  }
+
+  alerta() {
+    Swal.fire({
+      icon: 'success',
+      title: 'Imagen subida',
+    });
+  }
 }

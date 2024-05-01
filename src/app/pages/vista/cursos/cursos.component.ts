f12a246a188c7610bab7b378d2d9e0721c7e9c14
Jeans <sistemadesignstyle@gmail.com>
2024-04-30 23:00:19 -0500

    section color gradient

--- src/app/pages/vista/cursos/cursos.component.ts
+++ src/app/pages/vista/cursos/cursos.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CardCursoComponent } from 'src/app/layout/componentes/card-curso/card-curso.component';
 import { CardEspecialidadComponent } from 'src/app/layout/componentes/card-especialidad/card-especialidad.component';
+import { JumbotronImagenComponent } from 'src/app/layout/componentes/jumbotron-imagen/jumbotron-imagen.component';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [CardCursoComponent, CardEspecialidadComponent],
+  imports: [
+    CardCursoComponent,
+    CardEspecialidadComponent,
+    JumbotronImagenComponent,
+  ],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css',
 })

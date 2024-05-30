c61f19b40f1c031c712b0477efe117d9ddae7f63
Jeans <sistemadesignstyle@gmail.com>
2024-05-30 00:17:31 -0500

    instalacion font awesome

--- src/app/layout/componentes/footer-1/footer-1.component.ts
+++ src/app/layout/componentes/footer-1/footer-1.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Jumbotron1Component } from '../jumbotron-1/jumbotron-1.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHand } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-footer-1',
   standalone: true,
-  imports: [RouterLink, Jumbotron1Component],
+  imports: [RouterLink, Jumbotron1Component, FontAwesomeModule],
   templateUrl: './footer-1.component.html',
   styleUrl: './footer-1.component.css',
 })
-export class Footer1Component {}
+export class Footer1Component {
+  faHand = faHand;
+}

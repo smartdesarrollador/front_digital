60b991ef0ee72e608262aa293f81cda505ac19da
Jeans <sistemadesignstyle@gmail.com>
2025-01-11 16:01:32 -0500

    modificando cards slick

--- src/app/layout/componentes/slick-carousel/slick-carousel.component.ts
+++ src/app/layout/componentes/slick-carousel/slick-carousel.component.ts
@@ -77,13 +77,17 @@ export class SlickCarouselComponent implements OnInit, AfterViewInit {
               settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
+                arrows: true,
+                dots: true,
               },
             },
             {
               breakpoint: 1024,
               settings: {
-                slidesToShow: 4,
+                slidesToShow: 3,
                 slidesToScroll: 1,
+                arrows: true,
+                dots: true,
               },
             },
           ],
